3a4f4241fcb649d202e9742d69c2af32e3ea9a88
Cory <csaylor@analogds.com>
2025-03-17 20:28:00 -0400

    Implemented Fstrings, main(), input validation, and rounding

--- dice.py
+++ dice.py
@@ -1,11 +1,23 @@
 from random import randint
-total=0
-print("How many dice would you like to roll?")
-repeat=int(input())
-def Roll():
-	return randint(1,6)
-for i in range(repeat):
-	roll=Roll()
-	print ("Dice ",i+1,": ",roll)
-	total+=roll
-print("You rolled an average of ",total/repeat,"in ",repeat," rolls!")
+def roll():
+        """Generates a random number from 1-6"""
+        return randint(1,6)
+def main():
+	"""Runs "roll()" a user specified ammount of times, printing each result and the average at the end"""
+	total=0
+	print("How many dice would you like to roll?")
+	repeat=0
+	while (not (isinstance(repeat,int)) or (repeat <=0)):
+		try:
+			repeat=int(input())
+		except ValueError:
+			print("Please enter an integer.")
+		else:
+			print("Please enter a positive number.")
+	for i in range(repeat):
+		Roll=roll()
+		print (f"Dice {i+1}: {Roll}")
+		total+=Roll
+	print(f"You rolled an average of {total/repeat:.3f} in {repeat} rolls!")
+if __name__ == "__main__":
+	main()
